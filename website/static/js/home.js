3930fab9fec8f6692a4e56ed6f7ac070263953c2
pgk95 <kalupgk@gmail.com>
2023-12-18 21:15:23 +0300

    added some javascript to the template for more functionality

---
+++ website/static/js/home.js
@@ -0,0 +1,60 @@
+function validatePassword() {
+    var password = document.getElementById("password").value;
+    var confirmPassword = document.getElementById("confirmPassword").value;
+    var passwordHelp = document.getElementById("passwordHelp");
+
+    // Check if password meets criteria
+    var isPasswordValid = true;
+
+    // Minimum 8 characters
+    if (password.length < 8) {
+        passwordHelp.textContent = "Password must be at least 8 characters long";
+        isPasswordValid = false;
+    } else {
+        passwordHelp.textContent = "";
+    }
+
+    // Additional criteria here
+    var hasUppercase = /[A-Z]/.test(password);
+    var hasLowercase = /[a-z]/.test(password);
+    var hasDigit = /\d/.test(password);
+    var hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(password);
+
+    if (
+        !hasUppercase ||
+        !hasLowercase ||
+        !hasDigit ||
+        !hasSpecialChar
+    ) {
+        passwordHelp.textContent = "Password must contain at least one uppercase, one lowercase, one digit, and one special character";
+        isPasswordValid = false;
+    } else if (isPasswordValid) {
+        passwordHelp.textContent = "";
+    }
+
+    // Check if passwords match
+    if (password !== confirmPassword) {
+        passwordHelp.textContent = "Passwords do not match";
+        isPasswordValid = false;
+    }
+
+    return isPasswordValid;
+}
+
+
+// toggle password visibility
+function togglePasswordVisibility() {
+    var passwordInput = document.getElementById("password");
+    var confirmPasswordInput = document.getElementById("confirmPassword");
+    var visibilityToggle = document.getElementById("visibilityToggle");
+
+    if (passwordInput.type === "password") {
+      passwordInput.type = "text";
+      confirmPasswordInput.type = "text";
+      visibilityToggle.textContent = "Hide Password";
+    } else {
+      passwordInput.type = "password";
+      confirmPasswordInput.type = "password";
+      visibilityToggle.textContent = "Show Password";
+    }
+  }
